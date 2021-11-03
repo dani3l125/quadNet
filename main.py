91a4eaa50c3fef892c62603a760c288814172cd0
Daniel Yakovlev <daniel.ya.125@gmail.com>
2021-11-04 00:05:14 +0200

    preparing for training

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import random
 import csv
 import os.path as path
+import numpy as np
+import torch
 import torchvision.models as models
 
 r_min = -10000.0
 r_max = 10000.0
 header_p = ['a', 'b', 'c']
 header_r = ['root1', 'root2']
+par_path = 'Data/parameters.csv'
+roots_path = 'Data/roots.csv'
 
 def main():
-    if not path.exists('Data/parameters.csv'):  # generate dataset if needed
+    if not path.exists(par_path):  # generate dataset if needed
         # TODO: add one root, no roots. deal with loss function
-        with open('Data/parameters.csv', 'w', encoding='UTF8') as p, open('Data/roots.csv', 'w', encoding='UTF8') as r:
+        with open(par_path, 'w', encoding='UTF8') as p, open('Data/roots.csv', 'w', encoding='UTF8') as r:
             writer_r = csv.writer(r)
             writer_r.writerow(header_r)
             writer_p = csv.writer(p)
@@ -26,18 +30,26 @@ def main():
                 a = 1
                 b = r1 + r2
                 c = r1 * r2
-                if random.getrandbits(1): # can remove
+                if random.getrandbits(1):  # possible to remove 'if'
                     a = random.uniform(r_min, r_max)
                     b *= a
                     c *= a
-                writer_p.writerow([r1, r2])
-                writer_r.writerow([a, b, c])
+                writer_r.writerow([r1, r2])
+                writer_p.writerow([a, b, c])
 
     #TODO: visualize with pandas
 
     # Create network
     net = models.resnet18()  #TODO find best model
 
+    # Prepare the data
+    # with open(par_path, 'w', encoding='UTF8') as p, open('Data/roots.csv', 'w', encoding='UTF8') as r:
+    #     input_reader = csv.reader(p)
+    #     output_reader = csv.reader(r)
+    inputs_file = torch.from_numpy(np.loadtxt(par_path, dtype=np.float32, delimiter=",", skiprows=1))
+    outputs_file = torch.from_numpy(np.loadtxt(roots_path, dtype=np.float32, delimiter=",", skiprows=1))
+    print(inputs_file)
+    print(outputs_file)
 
 if __name__ == "__main__":
     main()
