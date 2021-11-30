f38f7a3fa7bb5c1f96276fd4a2de104781104abd
danielgre134 <daniel.greenhut@gmail.com>
2021-11-30 11:00:44 +0200

    Update QuadSet.py

--- dataUtils/QuadSet.py
+++ dataUtils/QuadSet.py
@@ -10,42 +10,41 @@ import pandas as pd
 
 r_min = -10000.0
 r_max = 10000.0
-header_p = ['a', 'b', 'c']
-header_r = ['roots']
-par_path = os.path.join("Data", "quadratic.csv")
+headers = ['coefficients', 'root1', 'root2']
+data_path = os.path.join("Data", "quadratic.csv")
 
 
 class Quadset(Dataset):
     def __init__(self, size=100000):
-        self.samples, self.roots_n = self.genData(size)
+        self.samples, self.roots = self.getData(size)
 
     def __len__(self):
         len(self.samples)
         return len(self.samples)
 
     def __getitem__(self, idx):
-        return [self.samples[idx], self.roots_n[idx]]
+        return [self.samples[idx], self.roots[idx]]
 
     def genData(self, size=100000):
-        if True or not path.exists(par_path) or not path.exists(roots_path):
-            if path.exists(par_path):
-                os.remove(par_path)
-            if path.exists(roots_path):
-                os.remove(roots_path)
-            with open(par_path, 'w', encoding='UTF8') as p, open(roots_path, 'w', encoding='UTF8') as r:
-                writer_r = csv.writer(r)
-                writer_r.writerow(header_r)
-                writer_p = csv.writer(p)
-                writer_p.writerow(header_p)
+        if True or not path.exists(data_path):
+            if path.exists(data_path):
+                os.remove(data_path)
+            with open(data_path, 'w', encoding='UTF8') as data:
+                writer = csv.writer(data)
+                writer.writerow(headers)
                 for i in range(int(size)):
                     a = random.uniform(r_min, r_max)
                     b = random.uniform(r_min, r_max)
                     c = random.uniform(r_min, r_max)
                     par_l = [a, b, c]
                     root_l = np.roots(par_l)
-                    writer_r.writerow(
-                        [root_l[0] if np.isreal(root_l[0]) else None, root_l[1] if np.isreal(root_l[1]) else None])
-                    writer_p.writerow(par_l)
-        inputs = pd.read_csv(par_path).dropna().reset_index(drop=True)
-        outputs = pd.read_csv(roots_path).dropna().reset_index(drop=True)
-        return inputs, outputs
+                    writer.writerow(
+                        [par_l, root_l[0] if np.isreal(root_l[0]) else None,
+                         root_l[1] if np.isreal(root_l[1]) else None])
+
+    def getData(self, size=100000):
+        data = pd.read_csv(path).dropna().reset_index(drop=True).head(size)
+        par = data[:, headers[0]]
+        root1 = data[:, headers[1]]
+        root2 = data[:, headers[2]]
+        return par, np.array(root1, root2).T
